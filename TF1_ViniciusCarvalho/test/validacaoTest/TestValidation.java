6c480b2c84815b7f8e529faf7d84adfdf31e726d
Vinicius Carvalho <vinisilvacar@gmail.com>
2016-07-20 21:08:17 -0300

    Continuação teste - validação Data

--- TF1_ViniciusCarvalho/test/validacaoTest/TestValidation.java
+++ TF1_ViniciusCarvalho/test/validacaoTest/TestValidation.java
@@ -9,14 +9,14 @@ import validacao.Validacao;
 public class TestValidation {
 
 	@Test
-	public void testVerificarRetornoString(){
+	public void testVerificarRetornoString() {
 		
 		String testName = null;
 		StringBuilder returnName = new StringBuilder();
 		
 		testName = "VINICIUS";
 		returnName = Validacao.validaNome(testName);
-		assertEquals("Teste de Nome válido!", testName, returnName.toString());
+		assertEquals("Teste de Nome valido!", testName, returnName.toString());
 		
 		testName = "Vinicius";
 		returnName = Validacao.validaNome(testName);
@@ -29,6 +29,20 @@ public class TestValidation {
 		testName = "";
 		returnName = Validacao.validaNome(testName);
 		assertNull(returnName);
+	}
+	
+	public void testVerificarDataValida() {
+		
+		String testData = null;
+		String returnData = null;
+		
+		testData = "01/01/2001";
+		returnData = Validacao.validaData(testData);
+		assertEquals("Teste de Data valido!", testData, returnData));
+		
+		testData = "30/01/2016";
+		returnData = Validacao.validaData(testData);
+		assertEquals("Teste de Data valido!", testData, returnData));
 	}
 
 }
